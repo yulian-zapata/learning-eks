32351a2510bc57c33dd9f9cdf6e0902b6dc67124
Ubuntu <ubuntu@ip-172-31-16-107.us-east-2.compute.internal>
2020-12-15 13:54:14 +0000

    fix

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -15,18 +15,18 @@ module "eks" {
   worker_groups = [
     {
       name                          = "worker-group-1"
-      instance_type                 = "t2.small"
+      instance_type                 = "t2.nano"
       additional_userdata           = "echo foo bar"
-      asg_desired_capacity          = 2
+      asg_desired_capacity          = 1
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
     },
-    {
-      name                          = "worker-group-2"
-      instance_type                 = "t2.medium"
-      additional_userdata           = "echo foo bar"
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
-      asg_desired_capacity          = 1
-    },
+#    {
+#      name                          = "worker-group-2"
+#      instance_type                 = "t2.micro"
+#      additional_userdata           = "echo foo bar"
+#      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
+#      asg_desired_capacity          = 1
+#    },
   ]
 }
 
